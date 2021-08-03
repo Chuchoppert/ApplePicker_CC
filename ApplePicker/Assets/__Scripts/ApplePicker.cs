c3965c85a6f2e041a1f096f5a6acfc29feca7ee4
Chuchoppert <chucho.tokayo@hotmail.com>
2021-08-02 19:14:08 -0500

    Basics of Applepicker
    set the basic lines to ApplePicker Script

--- ApplePicker/Assets/__Scripts/ApplePicker.cs
+++ ApplePicker/Assets/__Scripts/ApplePicker.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ApplePicker : MonoBehaviour
 {
+    [Header("Set in Inspector")]
+    public GameObject       ApplePrefab;
+    public float            speed =1;
+    public float            leftAndRightEdge = 10f; //Distance to not touch the edge
+    public float            ChanceToChangeDirections = 0.1f;
+    public float            secondsBetweenAppleDrops = 0.5f;
+
     // Start is called before the first frame update
     void Start()
     {
